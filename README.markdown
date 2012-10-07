e748a20de651561d37e757197f23be27de03c935
Eloy Durán <eloy.de.enige@gmail.com>
2012-10-07 21:46:11 +0200

    Configure token on deploy.

--- README.markdown
+++ README.markdown
@@ -9,6 +9,7 @@ $ rake db:setup
 
 ```
 $ export ADMIN_PASSWORD [SECRET]
+$ export RAILS_SECRET_TOKEN [SECRET]
 $ export AWS_ACCESS_KEY_ID [SECRET]
 $ export AWS_SECRET_ACCESS_KEY [SECRET]
 ```
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SBTR::Application.config.secret_token = '0674994302536c5acfd21733297738152daea63b8910b4b1e33e5c1ef54ceea65877a8f6d72773b1cb4eca41e1cb92a9d8b24df0449da9208c33a3a1d6a2266c'
+SBTR::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
